e51ebabcdf21d5e5dc4e5858d77ea44ac0cb0304
museun <museun@outlook.com>
2019-04-13 15:33:14 -0400

    guard

--- parser/src/parsers/guard.rs
+++ parser/src/parsers/guard.rs
@@ -1,13 +1,10 @@
 use super::*;
 
+/// for parsing things like -> Foo. will drop the -> and return the Foo
 #[derive(Debug)]
 pub struct Guard<T, E>(pub T, pub E);
 
-impl<'a, T, E> Guard<T, E>
-where
-    T: Syntax<'a>,
-    E: Syntax<'a, Output = T::Output>,
-{
+impl<'a, T, E> Guard<T, E> {
     pub fn new(t: T, e: E) -> Self {
         Self(t, e)
     }
@@ -15,16 +12,48 @@ where
 
 impl<'a, T, E> Syntax<'a> for Guard<T, E>
 where
-    T: Syntax<'a>,
-    E: Syntax<'a, Output = T::Output>,
+    T: Syntax<'a, Output = E::Output>,
+    E: Syntax<'a>,
 {
-    type Output = T::Output;
+    type Output = E::Output;
 
     fn test(&mut self, parser: &Parser<'a>) -> bool {
-        unimplemented!()
+        parser.test(&mut self.1)
     }
 
     fn expect(&mut self, parser: &mut Parser<'a>) -> Result<Self::Output> {
-        unimplemented!()
+        let Guard(guard, val) = self;
+        parser.expect(guard)?;
+        parser.expect(val)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn guard() {
+        let mut syntax = Guard::new(
+            lexer::Token::Sigil(lexer::Sigil::Arrow),
+            lexer::Token::Identifier,
+        );
+
+        let input: diag::Text = "foo -> bar".into();
+        let filename = diag::FileName::new("guard");
+
+        let tokens = lexer::Lexer::new(&input, filename)
+            .into_iter()
+            .collect::<Vec<_>>();
+
+        let mut parser = crate::Parser::new(filename, &input, &tokens);
+
+        let tok = parser.expect(&mut lexer::Token::Identifier).unwrap();
+        assert_eq!(tok.value, lexer::Token::Identifier);
+        assert_eq!(parser.string_at(tok.span).unwrap(), "foo");
+
+        let tok = parser.expect(&mut syntax).unwrap();
+        assert_eq!(tok.value, lexer::Token::Identifier);
+        assert_eq!(parser.string_at(tok.span).unwrap(), "bar");
     }
 }
